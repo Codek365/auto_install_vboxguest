3e3fa33591f5f3626c79a38f36f08d8f43291c38
Khoa Nguyen <khoa.nguyen@persol.co.jp>
2018-03-09 14:35:37 +0700

    ADD SCRIPT INSTALL LAMPP ON CENTOS & #14

--- AUTO-LAMP-CENTOS-7.sh
+++ AUTO-LAMP-CENTOS-7.sh
@@ -1,59 +1,59 @@
 #!/bin/bash
-# echo "Start Mount VMs Folder"
+echo "Start Mount VMs Folder"
 
-# echo -ne '#####                     (33%)\r'
-# sleep 1
-# echo -ne '#############             (66%)\r'
-# sleep 1
-# echo -ne '#######################   (100%)\r'
-# echo -ne '\n'
-
-
-
-# if [ ! -d /var/www ]; then
-#     sudo mkdir  /var/www
-# 	sudo mkdir /var/www/html
-# fi
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
 
 
-# sudo mount -t vboxsf -o uid=1000,gid=1000 WWW-SHARE /var/www/html
 
-# echo "mount -t vboxsf -o uid=1000,gid=1000 WWW-SHARE /var/www/html" >> /etc/rc.local 
-# sudo chmod +x /etc/rc.d/rc.local
+if [ ! -d /var/www ]; then
+    sudo mkdir  /var/www
+	sudo mkdir /var/www/html
+fi
 
-# sudo setenforce 0
 
-# echo "Start Install Apache"
-# echo -ne '#####                     (33%)\r'
-# sleep 1
-# echo -ne '#############             (66%)\r'
-# sleep 1
-# echo -ne '#######################   (100%)\r'
-# echo -ne '\n'
+sudo mount -t vboxsf -o uid=1000,gid=1000 WWW-SHARE /var/www/html
 
-# sudo yum -y clean all 
-# sudo yum -y update
-# sudo yum -y install centos-release-scl.noarch
-# sudo yum -y install yum-utils && sudo yum-config-manager --enable remi-php71 
-# sudo yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo php-mbstring php-dom 
-# sudo firewall-cmd --permanent --add-port=80/tcp
-# sudo firewall-cmd --permanent --add-port=443/tcp
-# sudo firewall-cmd --reload
-# sudo systemctl start httpd
-# sudo systemctl enable httpd
+echo "mount -t vboxsf -o uid=1000,gid=1000 WWW-SHARE /var/www/html" >> /etc/rc.local 
+sudo chmod +x /etc/rc.d/rc.local
 
-# echo "Start Install Mysql"
-# echo -ne '#####                     (33%)\r'
-# sleep 1
-# echo -ne '#############             (66%)\r'
-# sleep 1
-# echo -ne '#######################   (100%)\r'
-# echo -ne '\n'
+sudo setenforce 0
 
-# sudo yum -y install mariadb-server mariadb && 
-# #sudo mysql_secure_installation
-# sudo systemctl start mariadb.service 
-# sudo systemctl enable mariadb.service
+echo "Start Install Apache"
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+
+sudo yum -y clean all 
+sudo yum -y update
+sudo yum -y install centos-release-scl.noarch
+sudo yum -y install yum-utils && sudo yum-config-manager --enable remi-php71 
+sudo yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo php-mbstring php-dom 
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=443/tcp
+sudo firewall-cmd --reload
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+echo "Start Install Mysql"
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+
+sudo yum -y install mariadb-server mariadb && 
+#sudo mysql_secure_installation
+sudo systemctl start mariadb.service 
+sudo systemctl enable mariadb.service
 
 
 echo "Start Config Apache"
@@ -68,7 +68,7 @@ echo -ne '\n'
 echo '
 <FilesMatch \.php$>	        
 	SetHandler application/x-httpd-php	
-</FilesMatch>' > /etc/httpd/conf/httpd.conf
+</FilesMatch>' >> /etc/httpd/conf/httpd.conf
 
 touch /etc/httpd/conf.d/vhost.conf
 
@@ -80,7 +80,7 @@ echo '
 <Directory "/var/www/html/public/>
 	DirectoryIndex index.html index.php
 	AllowOverride All
-</Directory>' > /etc/httpd/conf.d/vhost.conf
+</Directory>' >> /etc/httpd/conf.d/vhost.conf
  
 sudo service httpd restart
 echo "Done!"
