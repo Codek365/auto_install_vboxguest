3f9b1c84f16d2a1320186b6ff8c83d39bbcf9a69
Khoa Nguyen <khoa.nguyen@persol.co.jp>
2018-03-09 14:24:21 +0700

    ADD SCRIPT INSTALL LAMPP ON CENTOS & #10

--- AUTO-LAMP-CENTOS-7.sh
+++ AUTO-LAMP-CENTOS-7.sh
@@ -1,73 +1,80 @@
 #!/bin/bash
-echo "Start Mount VMs Folder"
+# echo "Start Mount VMs Folder"
 
-echo -ne '#####                     (33%)\r'
-sleep 1
-echo -ne '#############             (66%)\r'
-sleep 1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
-
-
-
-if [ ! -d /var/www ]; then
-    sudo mkdir  /var/www
-	sudo mkdir /var/www/html
-fi
+# echo -ne '#####                     (33%)\r'
+# sleep 1
+# echo -ne '#############             (66%)\r'
+# sleep 1
+# echo -ne '#######################   (100%)\r'
+# echo -ne '\n'
 
 
-sudo mount -t vboxsf -o uid=1000,gid=1000 WWW-SHARE /var/www/html
 
-echo "mount -t vboxsf -o uid=1000,gid=1000 WWW-SHARE /var/www/html" >> /etc/rc.local 
-sudo chmod +x /etc/rc.d/rc.local
-
-sudo setenforce 0
-
-echo "Start Install Apache"
-echo -ne '#####                     (33%)\r'
-sleep 1
-echo -ne '#############             (66%)\r'
-sleep 1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
-
-sudo yum -y clean all 
-sudo yum -y update
-sudo yum -y install centos-release-scl.noarch
-sudo yum -y install yum-utils && sudo yum-config-manager --enable remi-php71 
-sudo yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo php-mbstring php-dom 
-sudo firewall-cmd --permanent --add-port=80/tcp
-sudo firewall-cmd --permanent --add-port=443/tcp
-sudo firewall-cmd --reload
-sudo systemctl start httpd
-sudo systemctl enable httpd
-
-echo "Start Install Mysql"
-echo -ne '#####                     (33%)\r'
-sleep 1
-echo -ne '#############             (66%)\r'
-sleep 1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
-
-sudo yum -y install mariadb-server mariadb && 
-#sudo mysql_secure_installation
-sudo systemctl start mariadb.service 
-sudo systemctl enable mariadb.service
+# if [ ! -d /var/www ]; then
+#     sudo mkdir  /var/www
+# 	sudo mkdir /var/www/html
+# fi
 
 
-echo "Start Config Apache"
-echo -ne '#####                     (33%)\r'
-sleep 1
-echo -ne '#############             (66%)\r'
-sleep 1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
+# sudo mount -t vboxsf -o uid=1000,gid=1000 WWW-SHARE /var/www/html
+
+# echo "mount -t vboxsf -o uid=1000,gid=1000 WWW-SHARE /var/www/html" >> /etc/rc.local 
+# sudo chmod +x /etc/rc.d/rc.local
+
+# sudo setenforce 0
+
+# echo "Start Install Apache"
+# echo -ne '#####                     (33%)\r'
+# sleep 1
+# echo -ne '#############             (66%)\r'
+# sleep 1
+# echo -ne '#######################   (100%)\r'
+# echo -ne '\n'
+
+# sudo yum -y clean all 
+# sudo yum -y update
+# sudo yum -y install centos-release-scl.noarch
+# sudo yum -y install yum-utils && sudo yum-config-manager --enable remi-php71 
+# sudo yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo php-mbstring php-dom 
+# sudo firewall-cmd --permanent --add-port=80/tcp
+# sudo firewall-cmd --permanent --add-port=443/tcp
+# sudo firewall-cmd --reload
+# sudo systemctl start httpd
+# sudo systemctl enable httpd
+
+# echo "Start Install Mysql"
+# echo -ne '#####                     (33%)\r'
+# sleep 1
+# echo -ne '#############             (66%)\r'
+# sleep 1
+# echo -ne '#######################   (100%)\r'
+# echo -ne '\n'
+
+# sudo yum -y install mariadb-server mariadb && 
+# #sudo mysql_secure_installation
+# sudo systemctl start mariadb.service 
+# sudo systemctl enable mariadb.service
 
 
-touch /etc/httpd/conf.d/vhost.conf
+# echo "Start Config Apache"
+# echo -ne '#####                     (33%)\r'
+# sleep 1
+# echo -ne '#############             (66%)\r'
+# sleep 1
+# echo -ne '#######################   (100%)\r'
+# echo -ne '\n'
+
+
 echo "<FilesMatch \.php$>	        SetHandler application/x-httpd-php	</FilesMatch>" >> /etc/httpd/conf/httpd.conf
-cp vhost //etc/httpd/conf.d/
+touch /etc/httpd/conf.d/vhost.conf
+echo '<VirtualHost *:80>
+	    ServerName localhost
+	    DocumentRoot /var/www/html/public/
+	</VirtualHost>
+	<Directory "/var/www/html/public/>
+	    DirectoryIndex index.html index.php
+	    AllowOverride All
+	</Directory>' >> /etc/httpd/conf/httpd.conf
 
 
 echo "Install Composer"
