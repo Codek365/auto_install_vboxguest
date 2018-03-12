7c00fdff8da66515ad960a67671cefa4ac30fa68
Khoa Nguyen <khoa.nguyen@persol.co.jp>
2018-03-12 15:08:48 +0700

    update script #6

--- AUTO-LAMP-CENTOS-7.sh
+++ AUTO-LAMP-CENTOS-7.sh
@@ -23,7 +23,7 @@ sudo chmod +x /etc/rc.d/rc.local
 
 
 
-echo "Start Install Mysql"
+echo "Start Install Mysql & Httpd"
 echo -ne '#####                     (33%)\r'
 sleep 1
 echo -ne '#############             (66%)\r'
@@ -31,10 +31,13 @@ sleep 1
 echo -ne '#######################   (100%)\r'
 echo -ne '\n'
 
+sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 sudo yum -y install mariadb-server mariadb && 
 #sudo mysql_secure_installation
 sudo systemctl start mariadb.service 
 sudo systemctl enable mariadb.service
+sudo yum -y install httpd
 
 
 
@@ -49,10 +52,9 @@ echo -ne '\n'
 
 
 sudo yum -y clean all 
+sudo rm -rf /var/cache/yum
 sudo yum -y update
-sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-sudo yum -y install mod_php71w php71w-opcache
+sudo yum -y install php mod_php71w php71w-opcache
 
 
 sudo yum -y install yum-utils && sudo yum-config-manager --enable remi-php71 
