d5dec9b49133e6088bb505e6abc9cf48747228ad
Khoa Nguyen <khoa.nguyen@persol.co.jp>
2018-03-12 13:26:37 +0700

    update script #4

--- auto_install.sh
+++ auto_install.sh
@@ -7,12 +7,12 @@ echo -ne '#############             (66%)\r'
 sleep 1
 echo -ne '#######################   (100%)\r'
 echo -ne '\n'
-mount /dev/cdrom /mnt
+sudo mount /dev/cdrom /mnt
 cd /mnt
 
 echo "Start Install VBoxLinuxAdditions"
 sleep 5
 
-yum -y install kernel-devel-$(uname -r) kernel-core-$(uname -r) gcc dkms make bzip2 perl
+sudo yum -y install kernel-devel-$(uname -r) kernel-core-$(uname -r) gcc dkms make bzip2 perl
 ./VBoxLinuxAdditions.run
 reboot
